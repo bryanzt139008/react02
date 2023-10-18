14e8efe9b06ef685ed349b2ba72ac10ebe69b21d
Bryan Zapana <bryanbozt139008@gmail.com>
2023-10-18 18:29:40 -0500

    v5. Diseño App.js y AppForm

---
+++ src/componentes/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBJslhq0xQVknljSxgavEOfxfswWxDQktg",
+  authDomain: "appreact-24194.firebaseapp.com",
+  projectId: "appreact-24194",
+  storageBucket: "appreact-24194.appspot.com",
+  messagingSenderId: "543192115726",
+  appId: "1:543192115726:web:d4ec90292d7ddcd1ba2a5f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
