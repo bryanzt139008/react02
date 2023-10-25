690913baa3711c13b6b02ba710bed0b52aba230f
Bryan Zapana <bryanbozt139008@gmail.com>
2023-10-24 19:40:33 -0500

    v6. se conf firebase y se guardo un objeto

--- src/componentes/firebase.js
+++ src/componentes/firebase.js
@@ -1,17 +1,26 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+
+import { getAuth} from "firebase/auth";
+import { getFirestore} from "firebase/firestore";
+import { getStorage} from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBJslhq0xQVknljSxgavEOfxfswWxDQktg",
-  authDomain: "appreact-24194.firebaseapp.com",
-  projectId: "appreact-24194",
-  storageBucket: "appreact-24194.appspot.com",
-  messagingSenderId: "543192115726",
-  appId: "1:543192115726:web:d4ec90292d7ddcd1ba2a5f"
+  apiKey: "AIzaSyCmVme0D5urCe982qqc_TdfXacnf1Nhpmk",
+  authDomain: "appreact-b374a.firebaseapp.com",
+  projectId: "appreact-b374a",
+  storageBucket: "appreact-b374a.appspot.com",
+  messagingSenderId: "913390551570",
+  appId: "1:913390551570:web:d6dae0e41e61fb41548ecb"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage (app);
+
